c010bc8d17cd13cb5c7bd84c52c36464f4cfa7eb
Bhagyashri Yogesh Patil <bhagyashriyogeshpatil@gmail.com>
2024-08-28 09:31:03 +0000

    Implemented MethodsAsPropsParent component passing handleSignIn and handleSignOut methods as props to MethodsAsPropsChild. Added conditional rendering and buttons for sign in and sign out functionality.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,7 +11,7 @@ import EventBinding from "./components/EventBinding";
 import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
 import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
 import NestingComponents from "./components/NestingComponents";
-
+import MethodsAsPropsParent from "./components/MethodsAsPropsParent";
 
 function App() {
   return (
@@ -26,7 +26,8 @@ function App() {
       {/* <EventBinding /> */}
       {/* <ConditionalRenderingClass /> */}
       {/* <ConditionalRenderingFunctional connected={true} /> */}
-      <NestingComponents />
+      {/* <NestingComponents /> */}
+      <MethodsAsPropsParent />
     </div>
   );
 }
