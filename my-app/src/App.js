f14a37df68a5756462cd67b5cb4c33995762a61f
Bhagyashri Yogesh Patil <bhagyashriyogeshpatil@gmail.com>
2024-08-29 08:31:53 +0000

    Add LifeCyclesCDM and LifeCyclesCDMChild components with data fetching in componentDidMount

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,6 +14,7 @@ import NestingComponents from "./components/NestingComponents";
 import MethodsAsPropsParent from "./components/MethodsAsPropsParent";
 import RenderingLists from "./components/RenderingLists";
 import Clock from "./components/Clock";
+import LifeCyclesCDM from "./components/LifeCyclesCDM";
 
 function App() {
   return (
@@ -31,7 +32,8 @@ function App() {
       {/* <NestingComponents /> */}
       {/* <MethodsAsPropsParent /> */}
       {/* <RenderingLists /> */}
-      <Clock />
+      {/* <Clock /> */}
+      <LifeCyclesCDM />
     </div>
   );
 }
